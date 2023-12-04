b164b2aeb085e84d7e7463844a2bc54e5fb1c744
Marcus Aurelius <contact@a.aurelius.org>
2023-12-03 23:01:10 -0800

    Add initial completion script

---
+++ share/completion.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# shellcheck disable=SC2207
+
+
+function __dot_deploy {
+   local base
+   base=$(realpath "$(which dot)")
+   local database="${base%/*}/database"
+   local curr="${COMP_WORDS[$COMP_CWORD]}"
+
+   local -a opts=()
+   while IFS=$'\t' read -r src _ ; do
+      opts+=( "$src" )
+   done < "$database"
+
+   COMPREPLY=( $(compgen -W "${opts[*]}" -- "${curr}") )
+}
+
+
+function __dot {
+   local curr="${COMP_WORDS[$COMP_CWORD]}"
+
+   local -a cmds=(
+      add
+      deploy
+      tree
+      ls
+      validate
+      diff
+      hg
+   )
+
+   case "${COMP_WORDS[1]}" in
+      '')
+         COMPREPLY=( $(compgen -W "${cmds[*]}" -- "${curr}") ) ;;
+
+      add)
+         compopt -o default ;;
+
+      deploy)
+         __dot_deploy ;;
+   esac
+
+}
+
+complete -F __dot dot
