43772ef7e0e0830fb1012033f4e67a42a879c058
Marcus Aurelius <contact@a.aurelius.org>
2024-03-04 20:54:52 -0800

    rm: old, unused vim nonsense

--- dotfiles/romanus/nvim/ftplugin/lisp.lua
+++ dotfiles/romanus/nvim/ftplugin/lisp.lua
@@ -6,7 +6,8 @@ vim.opt_local.textwidth   = 80
 -- Vertical split, REPL on the right
 vim.g.slimv_repl_split = 4
 vim.g.slimv_ctags      = 'ctags -a *.lisp'
-vim.g.slimv_swank_cmd  =
-   ':tabnew | term sbcl '                ..
-      "--eval '(ql:quickload :swank)' "  ..
-      "--eval '(swank:create-server :dont-close t)' "
+vim.g.slimv_swank_cmd  = table.concat({
+   ':tabnew | term sbcl',
+   "--eval '(ql:quickload :swank)'",
+   "--eval '(swank:create-server :dont-close t)'",
+})
