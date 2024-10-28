5145805c6ec5cc48323fb3a54365cb42f53aa738
Marcus Aurelius <contact@a.aurelius.org>
2024-10-28 00:19:34 -0700

    feat: easier line continuations in steno files

--- dotfiles/global/nvim/ftplugin/steno_translation.lua
+++ dotfiles/global/nvim/ftplugin/steno_translation.lua
@@ -9,12 +9,6 @@ vim.bo.smartindent = false
 vim.bo.autoindent  = false
 vim.bo.textwidth   = 72
 
--- Little convoluted. If mid-way through typing a word, puts the
--- box-drawing character at the start of the line, and bumps the word to
--- the subsequent line, positioning the cursor back at EOL.
-key_set('i', 'jk', '<C-o>I│<CR><C-o>A', {buffer=true})
-key_set('i', 'kj', '<C-o>I┐<CR><C-o>A', {buffer=true})
-
 key_set('i', '<C-e>', '<C-o>5<C-e>', {buffer=true})
 key_set('i', '<C-y>', '<C-o>5<C-y>', {buffer=true})
 
@@ -35,23 +29,48 @@ end, {
    expr = true
 })
 
--- Spellcheck.
--- Uses extmarks to more accurately return to the original cursor
--- position. Even if the line was shifted by the `1z=` correction.
-key_set('i', '<C-h>', function()
+-- Little convoluted. If mid-way through typing a word, puts the
+-- box-drawing character at the start of the line, and bumps the word to
+-- the subsequent line, positioning the cursor back at EOL.
+--key_set('i', 'jk', '<C-o>I│<CR><C-o>A', {buffer=true})
+--key_set('i', 'kj', '<C-o>I┐<CR><C-o>A', {buffer=true})
+
+local function mark_and_return(fn)
    local cursor = vim.api.nvim_win_get_cursor(0)
    local cursor_row    = cursor[1] - 1
    local cursor_column = cursor[2]
 
-   local ns = vim.api.nvim_create_namespace('spellcheck')
+   local ns = vim.api.nvim_create_namespace('__')
    local id = vim.api.nvim_buf_set_extmark(0, ns, cursor_row, cursor_column, {})
 
-   vim.cmd(':norm [s')  --< previous misspelling
-   vim.cmd(':norm 1z=') --< accept 1st suggestion
+   fn(cursor_row, cursor_column)
 
    local mark = vim.api.nvim_buf_get_extmark_by_id(0, ns, id, {})
    vim.api.nvim_win_set_cursor(0, {
       mark[1] + 1,
       mark[2]
    })
+end
+
+key_set('i', 'kj', function()
+   mark_and_return(function(row, _)
+      local last_line = vim.api.nvim_buf_get_lines(0, row-1, row, true)
+      if (last_line[1] == '│') or (last_line[1] == '┐') then
+         vim.cmd(':norm O│')
+      else
+         vim.cmd(':norm O┐')
+      end
+   end)
 end, {buffer=true})
+
+-- Spellcheck.
+-- Uses extmarks to more accurately return to the original cursor
+-- position. Even if the line was shifted by the `1z=` correction.
+key_set('i', '<C-h>', function()
+   mark_and_return(function(_, _)
+      vim.cmd(':norm [s')  --< previous misspelling
+      vim.cmd(':norm 1z=') --< accept 1st suggestion
+   end)
+end, {buffer=true})
+
+
